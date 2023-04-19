0ad324faa0277b04255a6431decca7c9d28825c8
cdb513 <cdbryson@yahoo.com>
2023-04-19 15:32:52 -0500

    Update Final Project R Script.R

--- Final Project R Script.R
+++ Final Project R Script.R
@@ -2,3 +2,32 @@
 # Final Project STA 3375
 
 
+# Set Working Directory
+
+setwd("C:/Users/cdbry/Desktop/Sports Analytics Class/Final Project/Data")
+
+
+
+# Libraries
+
+library(tidyverse)
+
+
+# Load Data
+
+dat <- read.csv(file = "golf_data.csv")
+
+
+# Remove useless columns
+
+dat <- dat |> 
+  select(-c(Unnamed..2, Unnamed..3, Unnamed..4)) |> 
+  filter(season == c(2018:2022))
+
+
+
+
+
+
+
+
